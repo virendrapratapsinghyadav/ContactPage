a08b2cf9b9961c9748f44faaec0eed31d365b5ac
Virendra <narveer019@gmail.com>
2025-07-21 13:13:41 +0530

    ADDED home about and contact in anvigation component

--- src/components/ContactHeader.jsx
+++ src/components/ContactHeader.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const ContactHeader = () => {
   return (
     <div>
-      
+      <h1>CONTACT US</h1>
+      <p>LET'S CONNECT: WE'RE HERE TO HELP, AND WE'D LOVE TO HEAR FROM YOU! WHETHER YOU HAVE A QUESTION, COMMENT, OR JUST WANT TO CHAT, YOU CAN REACH OUT TO US THROUGH THE CONTACT FORM OF THIS PAGE, OR BY PHONE, EMAIL, OP SOCIAL MEDIA.</p>
     </div>
   )
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const Navigation = () => {
   return (
-    <div>
-      
+    <nav className=''>
+    <div className='logo'>
+      <img src="/images/logo.png" alt="Logo" />
     </div>
+    <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+    </ul>
+    </nav>
   )
 }
 
